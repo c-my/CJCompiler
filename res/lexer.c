c91c3b1d7fe80919d014ccc725492f52144c6226
c-my <cmy1113@outlook.com>
2018-12-22 10:08:48 +0800

    trying to fix break

--- res/lexer.c
+++ res/lexer.c
@@ -2,21 +2,7 @@ int  b = 13;
 int a = 20;
 while(1)
 {
-    a+=1;
-    if(a==30){
-        break;
-    }else{}
-}
-if(a<2)
-{
-    a-1;
-}
-else{
-a+=1;
-a-=2;
-a*=3;
-a/=4;
-a<<=1;
+    break;
 }
 
 
